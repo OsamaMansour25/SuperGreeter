17d5a0fb5606fbbe1be26528a42512d340b03a0b
OsamaMansour25 <osam0289@stud.kea.dk>
2022-09-09 10:26:19 +0200

    added askAboutAge

---
+++ src/SuperGreeterx.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SuperGreeterx {
+    public String start() {
+        String start = "Det virker!";
+        return start;
+    }
+
+    public static void main(String[] args) {
+
+        SuperGreeterx sG = new SuperGreeterx();
+        System.out.println(sG.start());
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast dit navn");
+        String navn;
+        navn = sc.nextLine();
+System.out.println("Hej" + " " + navn);
+
+int alder;
+System.out.println("Hvor gammel er du?");
+alder = sc.nextInt();
+System.out.println("Din alder er" + alder);
+    }
+    }
+
